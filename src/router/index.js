b3d0496eb798191d7c452095b97a2737fdf9f860
hapalpal <ehddnsla0102@naver.com>
2023-12-05 12:20:45 +0900

    design

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import UserInfo from "@/pages/MyPage/UserInfo.vue";
+import UpdateUserInfo from "@/pages/MyPage/UpdateUserInfo.vue";
+
+
 const router= createRouter({
     history: createWebHistory(),
+
     routes : [
         {path:'/userinfo',component:UserInfo},
-
+        {path:'/userinfo/updateuserinfo',component:UpdateUserInfo},
 
     ]
 })
