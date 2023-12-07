ddf46ae5f4ce3863690de371d39d2d19ae69cd60
jeon-aelim <devaelim@gmail.com>
2023-12-07 10:32:36 +0900

    Fixed: 채팅방 서버 변동에 맞춰 바꾸는중임

--- src/components/chat/ChatDetail.vue
+++ src/components/chat/ChatDetail.vue
@@ -1,11 +1,13 @@
 <template>
     <body>
         <div id='chatt'>
-            <input type='text' v-model='mid' placeholder="닉네임을 입력해 주세요">
+            <input type='text' v-model='sender' placeholder="닉네임을 입력해 주세요">
+            <input type='text' v-model='masterId' placeholder="방이름">
+            <input type='button' value='접속' @click="openSocket">
             <br />
             <div v-for="(item, index) in talk" :key="index" v-html="item"></div>
             <div id='sendZone'>
-                <textarea v-model='msg'></textarea>
+                <textarea v-model='contents'></textarea>
                 <input type='button' value='전송' @click="send">
             </div>
         </div>
@@ -13,21 +15,23 @@
     </body>
 </template> 
 <script setup>
-import {ref, onMounted} from 'vue';
+import {ref, onUnmounted} from 'vue';
 
-const mid = ref("");
+const sender = ref("");
+const masterId = ref("");
 let talk = ref([]);
-const msg = ref("");
+const contents = ref("");
+
 let ws;
-onMounted(() => {
+const openSocket = () => {
     // 기존 DB 채팅 내용 load
     
     // Connect WebSocket  
-    ws = new WebSocket("ws://" + "localhost:8089" + "/chat");
+    ws = new WebSocket("ws://localhost:8089/chat/" + masterId.value);
     ws.onmessage = function (onmessage) {
         let data = JSON.parse(onmessage.data);
         let css;
-        if (data.mid == mid.value) {
+        if (data.sender == sender.value) {
             // css = 'class=me';
             css = 'style=background:yellow;'
         } else {
@@ -35,26 +39,32 @@ onMounted(() => {
             css = 'style=background:gray;'
         }
         let item = `<div ${css}>
-<span><b>${data.mid}</b></span> [ ${data.date} ]<br/>
-<span>${data.msg}</span>
+<span><b>${data.sender}</b></span> [ ${data.created} ]<br/>
+<span>${data.contents}</span>
 </div>`;
 talk.value = [...talk.value, item]; // 배열에 추가
         // talk.scrollTop = talk.scrollHeight;//스크롤바 하단으로 이동
     }
-})
-
+}
 
 const send = () => {
     let data = {};
 
-    if (msg.value.trim() != '') {
-        data.mid = mid.value;
-        data.msg = msg.value;
-        data.token = sessionStorage.getItem("token");
-        data.date = new Date();
+    if (contents.value.trim() != '') {
+        data.sender = sender.value;
+        data.contents = contents.value;
+        // data.token = sessionStorage.getItem("token");
+        
         let temp = JSON.stringify(data);
         ws.send(temp);
     }
-    msg.value = '';
+    contents.value = '';
 }
+
+onUnmounted(() => {
+    if (ws) {
+        ws.close();
+    }
+});
+
 </script>
