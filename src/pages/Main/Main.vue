41becc4c529c119ae89d1c706388f82be1285c94
tlagusejr <starc13@naver.com>
2023-12-08 17:05:17 +0900

    Feat: Mainpage 라우터 연결

--- src/pages/Main/Main.vue
+++ src/pages/Main/Main.vue
@@ -1,6 +1,28 @@
+<script setup>
+
+
+import MainSideGroups from "@/components/MainSideGroups.vue";
+import {api} from "@/common.js";
+import {ref} from "vue";
+
+
+const data = ref([]);
+
+api("api/webtoon/webtoonlist?keyword=" + "recent",
+    "GET",
+).then((response) =>{
+  console.log(response)
+      data.value[0] = response;
+      console.log(data.value[0])
+    }
+);
+
+
+</script>
+
 <template>
   <div>
-    <main-side-groups :title="'최근 연재 시작한 웹툰'"></main-side-groups>
+    <main-side-groups :title="'최근 연재 시작한 웹툰'" :webtoons="data[0]"></main-side-groups>
   </div>
   <div>
     <main-side-groups :title="'정주행하기 좋은 완결 웹툰'"></main-side-groups>
@@ -22,11 +44,6 @@
 
 </script>
 
-<script setup>
-
-
-import MainSideGroups from "@/components/MainSideGroups.vue";
-</script>
 <style scoped >
 div {
   margin: 20px 0 20px 0;
