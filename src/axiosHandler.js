4bd87c529694f82bf72e2144c42a80df69bfea02
SEONGSOO LEE <babysoo123@gmail.com>
2023-12-14 17:11:56 +0900

    Refactor: axois 인터셉터 고치는중

--- src/axiosHandler.js
+++ src/axiosHandler.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { storeToRefs } from 'pinia';
 import {api} from "@/common.js";
+import {ref} from "vue";
 
 axios.defaults.withCredentials = true;
 
@@ -8,27 +9,35 @@ export function setupAxiosInterceptors(store){
     
     const { user } = storeToRefs(store);
 
+
     // Request Interceptor
     axios.interceptors.request.use((config) => {
-        //변수 설정
-        // alert("인터셉터 시작");
-        // alert(new URL(config.url).pathname);
-
-        const isLoggedIn = (null === user.value) ? false : true ;// 로그인 여부
+    /*    //변수 설정
+        const isLoggedIn = user.value !== null; // 사용자 로그인 여부 확인
         const publicApiPaths = import.meta.env.VITE_PUBLIC_API_PATHS.split(','); // 로그인이 필요하지 않은 API 목록
+        const requestPath = new URL(config.url).pathname;
+
+        // 요청 경로가 로그인이 필요한 API 경로에 포함되지 않으면서 로그인이 되어 있는 경우
+        if (isLoggedIn && !publicApiPaths.some(path => requestPath.startsWith(path))) {
+            config.headers['Authorization'] = `Bearer ${user.value.accessToken}`; // 헤더에 Access 토큰 추가
+        }
+
+        // 토큰 재발급 요청 시
+        if (requestPath === '/api/auth/accessoken') {
+            config.headers['Authorization'] = `Bearer ${user.value.refreshToken}`; // Refresh 토큰으로 요청 헤더 설정
+            config.headers['X-Refresh-Token'] = 'true';
+        }*/
+        const isLoggedIn = (null === user.value) ? false : true ;// 로그인 여부
+        const publicApiPaths = import.meta.env.VITE_PUBLIC_API_PATHS.split(',');// 로그인이 필요하지 않은 API 목록
+        // const isPublicApiPath = publicApiPaths.some(item => item.startsWith(new URL(config.url).pathname));
         const isPublicApiPath = publicApiPaths.includes(new URL(config.url).pathname); // 로그인이 필요하지 않은 API 여부
-        let token;
+        const token = ref('');
         if (isLoggedIn && !isPublicApiPath){
-            const tokenDto = JSON.parse(user.value.token);
-            token = tokenDto.accessToken;
-            // alert(token);
-            config.headers['Authorization'] = token;
+            config.headers['Authorization'] = user.value.accessToken;
         }
 
         if (config.url === `${import.meta.env.VITE_SERVER_URL}/api/auth/accessoken`){
-            const tokenDto = JSON.parse(user.value.token);
-            token = tokenDto.refreshToken;
-            config.headers['Authorization'] = token;
+            config.headers['Authorization'] = user.value.refreshToken;
             config.headers['X-Refresh-Token'] = 'true';
         }
 
@@ -43,25 +52,21 @@ export function setupAxiosInterceptors(store){
 
     // Response Interceptor
     axios.interceptors.response.use((response) => {
-        // alert("response 도착");
-        // console.log(response);
         return response;
     }, (error) => {
         const errResStatus = error.response.status;
         if (errResStatus === 401 || errResStatus === 403) {
-            alert("권한이 없습니다. 로그아웃 되었습니다. 다시 로그인해주세요.")
+            alert("권한이 없습니다. 로그아웃 되었습니다. 다시 로그인해주세요.");
             store.logout();
         }else if(errResStatus === 409){
-            alert("1.토큰 만료");
-            alert(error.response.data.refresh);
             if(error.response.data.refresh === "true"){
-                // alert("refresh 재발급 필요");
                 api("api/auth/accessoken", 'get', '').then(r  => {
-                    alert("then");
-                    alert(r);
-                    const tokenDto = JSON.parse(user.value.token);
-                    tokenDto.accessToken = "Bearer " + r.toString();
-                    alert("잠시중지");
+                    user.value.accessToken = "Bearer " + r.toString();
+                    alert("권한이 갱신되었습니다. 다시 시도 해주세요");
+
+                }).catch(e => {
+                    alert("권한이 만료되었습니다. 다시 로그인 해주세요.");
+                    store.logout();
                 });
             }
         }
