be48508ee5568b27e7fd556c0ff39245bf786b10
SEONGSOO LEE <babysoo123@gmail.com>
2023-12-20 10:33:32 +0900

    Refactor: 관리자 정보 수정

--- src/pages/board/createBoard.vue
+++ src/pages/board/createBoard.vue
@@ -39,7 +39,7 @@ const createBoard = async (board) => {
     for (let i = 0; i < board.boardFiles.length; i++) {
       formData.append('imageFiles', board.boardFiles[i]);
     }
-    formData.append("noticeYn", useAuthStore().user.usersRole === "ADMIN" ? 1 : 0);// 0: 자유 1: 공지
+    formData.append("noticeYn", useAuthStore().user.nickname === "관리자" || useAuthStore().user.nickname === "admin" ? 1 : 0);// 0: 자유 1: 공지
     formData.append("users", loginUsersId.value ); // users_id
     const response = await api("board", "POST", formData);
     if (response instanceof Error) {
