fa8e54488a28e6bbab0f44e369da9f1aca7faed7
tlagusejr <starc13@naver.com>
2023-12-23 20:17:04 +0900

    Feat: 통계 수정

--- src/components/admin/Barchart.vue
+++ src/components/admin/Barchart.vue
@@ -2,50 +2,22 @@
   <v-container>
     <v-row>
       <v-col
-        cols="6"
+          cols="6"
       >
-        <div
-        >
-          <Bar
-              id="my-chart-id1"
-              :options="chartOptions1"
-              :data="chartData1"
-          />
-        </div>
+        <canvas id="myChart1"></canvas>
       </v-col>
       <v-col
           cols="6"
       >
-        <div
-        >
-          <Bar
-              id="my-chart-id2"
-              :options="chartOptions2"
-              :data="chartData2"
-          />
-        </div>
+        <canvas id="myChart2"></canvas>
       </v-col>
       <v-col
           cols="6">
-        <div
-        >
-          <Bar
-              id="my-chart-id3"
-              :options="chartOptions3"
-              :data="chartData3"
-          />
-        </div>
+        <canvas id="myChart3"></canvas>
       </v-col>
       <v-col
           cols="6">
-        <div
-        >
-          <Bar
-              id="my-chart-id4"
-              :options="chartOptions4"
-              :data="chartData4"
-          />
-        </div>
+        <canvas id="myChart4"></canvas>
       </v-col>
 
     </v-row>
@@ -53,150 +25,138 @@
 
 </template>
 
-<script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+<script setup>
+import {Bar} from 'vue-chartjs'
+import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
 import {api} from "@/common.js";
-
+import Chart from 'chart.js/auto';
+import {onMounted, ref} from "vue";
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-const label1 = [0,0,0,0,0]
-const count1 = [0,0,0,0,0]
-const label2 = [0,0,0,0,0]
-const count2 = [0,0,0,0,0]
-const label3 = [0,0,0,0,0]
-const count3 = [0,0,0,0,0]
-const label4 = [0,0,0,0,0]
-const count4 = [0,0,0,0,0]
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
+const nickname1 = []
+const followNum1 = []
+const nickname2 = []
+const followNum2 = []
+const title3 = []
+const rate3 = []
+const title4 = []
+const likeCount4 = []
 
+onMounted(async () => {
+  const [response1, response2, response3, response4] = await Promise.all([
     api("admin/followtop",
         "GET").then(
-        (response) => {
-          for (const idx in response.content) {
-            label1[idx] = response.content[idx].usersNickname
-            count1[idx] = response.content[idx].followCount
-          }
-        }
-    )
+        (response) => response
+    ),
     api("admin/followingtop",
         "GET").then(
-        (response) => {
-          for (const idx in response.content) {
-            label2[idx] = response.content[idx].usersNickname
-            count2[idx] = response.content[idx].followCount
-          }
-        }
-    )
+        (response) => response
+    ),
     api("admin/webtoonratetop",
         "GET").then(
-        (response) => {
-          for (const idx in response.content) {
-            label3[idx] = response.content[idx].title
-            count3[idx] = Number.parseFloat(response.content[idx].rate)/2
-          }
-        }
-    )
+        (response) => response
+    ),
     api("admin/webtoonliketop",
         "GET").then(
-        (response) => {
-          for (const idx in response.content) {
-            label4[idx] = response.content[idx].title
-            count4[idx] = response.content[idx].likeCount
-          }
-        }
+        (response) => response
     )
+  ])
 
-    return {
-      chartData1: {
-        labels: label1,
-        datasets: [
-            { data: count1,
-              label: '유저',
-              backgroundColor:['#5041BC','#8F7CEE','#8F7CEE','#8F7CEE','#8F7CEE',]
-            } ]
-      },
-      chartOptions1: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: '팔로우 가장 많이 한 유저',
-          },
-          deferred: {
-            delay: 500
-          },
-        },
-      },
-      chartData2: {
-        labels: label2,
-        datasets: [
-          { data: count2,
-            label: '유저',
-            backgroundColor:['#11235A','#596FB7','#596FB7','#596FB7','#596FB7',]
-          } ]
-      },
-      chartOptions2: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: '가장 많이 팔로윙 된 유저',
-          },
-          deferred: {
-            delay: 500
-          },
-        }
-      },
-      chartData3: {
-        labels: label3,
-        datasets: [
-          { data: count3,
-            label: '웹툰',
-            backgroundColor:['#C21292','hotpink','hotpink','hotpink','hotpink',]
-          } ]
-      },
-      chartOptions3: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: '가장 평점 좋은 웹툰',
-          },
-          deferred: {
-            delay: 500
-          },
-        }
-      },
-      chartData4: {
-        labels: label4,
-        datasets: [
-          { data: count4,
-            label: '웹툰',
-            backgroundColor:['red','#EA906C','#EA906C','#EA906C','#EA906C',]
-          } ]
-      },
-      chartOptions4: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: '즐겨찾기 많이 된 웹툰',
-          },
-          deferred: {
-            delay: 500
-          },
+  for (const idx in response1.content) {
+    nickname1[idx] = response1.content[idx].usersNickname
+    followNum1[idx] = response1.content[idx].followCount
+  }
+  for (const idx in response2.content) {
+    nickname2[idx] = response2.content[idx].usersNickname
+    followNum2[idx] = response2.content[idx].followCount
+  }
+  for (const idx in response3.content) {
+    title3[idx] = response3.content[idx].title
+    rate3[idx] = (response3.content[idx].rate)/2
+  }
+  for (const idx in response4.content) {
+    title4[idx] = response4.content[idx].title
+    likeCount4[idx] = response4.content[idx].likeCount
+  }
+
+
+
+  const ctx1 = document.getElementById('myChart1');
+  const ctx2 = document.getElementById('myChart2');
+  const ctx3 = document.getElementById('myChart3');
+  const ctx4 = document.getElementById('myChart4');
+
+  new Chart(ctx1, {
+    type: 'bar',
+    data: {
+      labels: nickname1,
+      datasets: [{
+        label: '팔로우 제일 많이 한 사람',
+        data: followNum1,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
         }
       }
     }
-  }
-}
-
-
-
-
-
+  });
+  new Chart(ctx2, {
+    type: 'bar',
+    data: {
+      labels: nickname2,
+      datasets: [{
+        label: '팔로우 제일 많이 된 사람',
+        data: followNum2,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  new Chart(ctx3, {
+    type: 'bar',
+    data: {
+      labels: title3,
+      datasets: [{
+        label: '평균 별점 순위',
+        data: rate3,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  new Chart(ctx4, {
+    type: 'bar',
+    data: {
+      labels: title4,
+      datasets: [{
+        label: '가장 많이 즐겨찾기 된 웹툰',
+        data: likeCount4,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+})
 </script>
